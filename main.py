c6265ac41f767b81cf89b2a6f36a1c9802469a17
iman01 <iman.esfandiyar71@gmail.com>
2023-03-02 10:22:09 +0100

    Convert DMS2DD added

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-# This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-import pandas as pd
 import geopandas as gpd
 import matplotlib.pyplot as plt
-from shapely import wkt
-import numpy as np
+import pandas as pd
+import re
+
 def rgb_to_hex(rgb):
     return '%02x%02x%02x' % rgb
 # Press the green button in the gutter to run the script.
+
+
 if __name__ == '__main__':
     csv_data_gps = pd.read_csv('Dane_pozycja_GPS.csv')  # read gps data
     csv_data_gps.columns = ['time', 'lat', 'lat1', 'lat2', 'lat3', 'lon', 'lon1', 'lon2', 'lon3']  # naming columns
@@ -20,27 +19,22 @@ if __name__ == '__main__':
 
     csv_data = csv_data[:-1]  # drop the last raw as it's nan
 
+    #Convert DMS2DD - degree minute second to degree decimal
     data_gps = pd.DataFrame(columns=['latitude', 'longitude'])
-    data_gps['latitude'] = csv_data_gps['lat'] + (csv_data_gps['lat1'] * 0.01) + (csv_data_gps['lat2'] * 0.0001) + (
-            csv_data_gps['lat3'] * 0.000001)
-    data_gps['longitude'] = csv_data_gps['lon'] + (csv_data_gps['lon1'] * 0.01) + (csv_data_gps['lon2'] * 0.0001) + (
-            csv_data_gps['lon3'] * 0.000001)
+    data_gps['latitude'] = csv_data_gps['lat'] + (csv_data_gps['lat1'] / 60) + (csv_data_gps['lat2'] / 3600) + (
+            csv_data_gps['lat3'] / 360000 )
+    data_gps['longitude'] = csv_data_gps['lon'] + (csv_data_gps['lon1'] /60 ) + (csv_data_gps['lon2'] / 3600) + (
+            csv_data_gps['lon3'] / 360000)
 
     data_gps = data_gps[:-1]  # drop the last raw as it's nan
 
     data_gdf = gpd.GeoDataFrame(data_gps, geometry=gpd.points_from_xy(data_gps['longitude'], data_gps['latitude']))
-    this_color_c = []
-    for index, raw in data_gdf.iterrows():
-        # this_color_c.append('#D94325')
-        this_color_c.append(rgb_to_hex((12, 12, 12)))
-        print(index)
 
 
-    data_gdf['color'] = this_color_c
-    print(data_gdf['color'])
-    data_gdf.plot(color=data_gdf['color'])
-    print(data_gdf.plot())
+
+
+    # print(data_gdf['color'])
+
+    data_gdf.plot()
     plt.show()
-    print(rgb_to_hex((12, 12, 12)))
-    print(rgb_to_hex((12, 12, 12)))
     data_gdf.to_file(filename="data.shp", driver='ESRI Shapefile')

21e50ed453143368079619bf7b62baa13054e9e0
iman01 <iman.esfandiyar71@gmail.com>
2023-03-05 08:46:06 +0100

    heatmap added

--- main.py
+++ main.py
@@ -1,40 +1,53 @@
 
-
+# use https://equatorstudios.com/shapefile-viewer to view shape file
 import geopandas as gpd
 import matplotlib.pyplot as plt
 import pandas as pd
-import re
-
-def rgb_to_hex(rgb):
-    return '%02x%02x%02x' % rgb
-# Press the green button in the gutter to run the script.
+import matplotlib.colors as colors
+import matplotlib as mpl
+import folium
+import mapclassify
+import fiona
 
 
 if __name__ == '__main__':
     csv_data_gps = pd.read_csv('Dane_pozycja_GPS.csv')  # read gps data
+    #TODO: change label to correct names
     csv_data_gps.columns = ['time', 'lat', 'lat1', 'lat2', 'lat3', 'lon', 'lon1', 'lon2', 'lon3']  # naming columns
     csv_data = pd.read_csv('Dane_razem.csv')  # read sensor data
     csv_data.columns = ['time', 'column1', 'column2', 'column3', 'column4', 'column5', 'column6', 'column7',
                         'column8']  # naming columns
 
     csv_data = csv_data[:-1]  # drop the last raw as it's nan
+    csv_data_gps = csv_data_gps[:-1] # drop the last raw as it's nan
 
-    #Convert DMS2DD - degree minute second to degree decimal
-    data_gps = pd.DataFrame(columns=['latitude', 'longitude'])
+    # Convert DMS2DD - degree minute second to degree decimal
+    data_gps = pd.DataFrame(columns=['latitude', 'longitude', 'height'])
     data_gps['latitude'] = csv_data_gps['lat'] + (csv_data_gps['lat1'] / 60) + (csv_data_gps['lat2'] / 3600) + (
             csv_data_gps['lat3'] / 360000 )
-    data_gps['longitude'] = csv_data_gps['lon'] + (csv_data_gps['lon1'] /60 ) + (csv_data_gps['lon2'] / 3600) + (
+    data_gps['longitude'] = csv_data_gps['lon'] + (csv_data_gps['lon1'] / 60) + (csv_data_gps['lon2'] / 3600) + (
             csv_data_gps['lon3'] / 360000)
 
-    data_gps = data_gps[:-1]  # drop the last raw as it's nan
+    data_gps['height'] = (csv_data['column3'] * 256) + csv_data['column4'] # Max 658, min 0
+
+
+
+
+
+
+
+
+
 
     data_gdf = gpd.GeoDataFrame(data_gps, geometry=gpd.points_from_xy(data_gps['longitude'], data_gps['latitude']))
 
-
-
-
     # print(data_gdf['color'])
 
-    data_gdf.plot()
+
+    data_gdf.plot(column='height', cmap=mpl.colormaps['afmhot'], vmin = min(data_gps['height']),
+                  vmax = max(data_gps['height']))
+
+
+    data_gdf.explore(column='height')
     plt.show()
     data_gdf.to_file(filename="data.shp", driver='ESRI Shapefile')
